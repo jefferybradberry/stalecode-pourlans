29e703e754d6344f1cd9f7f0eff59864a2d539d7
Jeffery Bradberry <jefferybradberry@stalecode.com>
2016-08-04 22:44:54 -0400

    (work in progress)

---
+++ src/main/java/com/stalecode/pourlans/Pourlans.java
@@ -0,0 +1,15 @@
+package com.stalecode.pourlans;
+
+public class Pourlans implements Runnable {
+
+	public static void main(final String[] args) {
+		new Pourlans().run();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
